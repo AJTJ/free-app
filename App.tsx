dcbd3f2d8b353c3c2f4b5308dbabb421b9589904
AJTJ <janke.aaron@gmail.com>
2022-09-05 00:10:15 -0700

    added graphql, began integrating debugger

--- App.tsx
+++ App.tsx
@@ -6,6 +6,13 @@ import { RecoilRoot } from "recoil";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Auth } from "./pages";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 // TODO: Figure out the typing of React Navigator
 type RootStackParamList = {
   Home: undefined;
@@ -16,13 +23,15 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <RecoilRoot>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Auth">
-          <Stack.Screen name="Auth" component={Auth} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </RecoilRoot>
+    <ApolloProvider client={client}>
+      <RecoilRoot>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Auth">
+            <Stack.Screen name="Auth" component={Auth} />
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </RecoilRoot>
+    </ApolloProvider>
   );
 }
--- NOTES.md
+++ NOTES.md
@@ -1,8 +1,22 @@
+### desktop tools
+- react-native-debugger
+  - this seems useful in order to make the best use of apollo graphql
+  - install: https://github.com/jhen0409/react-native-debugger
+
+### TODOs
+- styling library?
+- data visualization library?
+- graphql integration library
+- debugging
+  - https://reactnative.dev/docs/debugging
+    - get this working iwth the `react-native-debugger` tool
+    - referenced here: https://www.apollographql.com/docs/react/integrations/react-native/
+
 ### commands
 - start project for dev (with phone)
   - `npx expo start`
 
-### TOOLS
+### project tools
 - state management
   - recoil
     - https://recoiljs.org/docs/introduction/getting-started
@@ -14,10 +28,10 @@
   - formik
     - https://formik.org/docs/guides/typescript
     - https://formik.org/docs/guides/react-native
+- graphql
+  - apollo graphql client
+    - https://www.apollographql.com/docs/react/integrations/react-native/
+    - https://www.apollographql.com/docs/react/development-testing/static-typing/
 - eslint rules, but for typescript:
   - https://typescript-eslint.io/
-
-### TODOs
-- styling library?
-- data visualization library?
 

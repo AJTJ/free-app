8ac0eca1afe4136ce636fd7139af404f9e234aaa
AJTJ <janke.aaron@gmail.com>
2023-06-08 14:58:25 -0600

    update Thu  8 Jun 2023 14:58:25 CST

--- api/auth.generated.ts
+++ api/auth.generated.ts
@@ -1,109 +1,15 @@
-import * as Types from "./types/types.generated";
+import * as Types from './types/types.generated';
 
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
-export type UserItemFragment = {
-  __typename?: "UserQueryData";
-  email: string;
-  userId: any;
-  username: string;
-};
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type UserItemFragment = { __typename?: 'UserQueryData', email: string, userId: any, username: string };
 
 export type LoginQueryVariables = Types.Exact<{
-  email: Types.Scalars["String"]["input"];
-  password: Types.Scalars["String"]["input"];
+  email: Types.Scalars['String']['input'];
+  password: Types.Scalars['String']['input'];
 }>;
 
-export type LoginQuery = {
-  __typename?: "QueryRoot";
-  user: {
-    __typename?: "UserQueryData";
-    email: string;
-    userId: any;
-    username: string;
-  };
-};
 
-export const LoginDocument = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "OperationDefinition",
-      operation: "query",
-      name: { kind: "Name", value: "login" },
-      variableDefinitions: [
-        {
-          kind: "VariableDefinition",
-          variable: {
-            kind: "Variable",
-            name: { kind: "Name", value: "email" },
-          },
-          type: {
-            kind: "NonNullType",
-            type: {
-              kind: "NamedType",
-              name: { kind: "Name", value: "String" },
-            },
-          },
-        },
-        {
-          kind: "VariableDefinition",
-          variable: {
-            kind: "Variable",
-            name: { kind: "Name", value: "password" },
-          },
-          type: {
-            kind: "NonNullType",
-            type: {
-              kind: "NamedType",
-              name: { kind: "Name", value: "String" },
-            },
-          },
-        },
-      ],
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "user" },
-            arguments: [
-              {
-                kind: "Argument",
-                name: { kind: "Name", value: "queryEmail" },
-                value: {
-                  kind: "Variable",
-                  name: { kind: "Name", value: "email" },
-                },
-              },
-            ],
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [
-                {
-                  kind: "FragmentSpread",
-                  name: { kind: "Name", value: "UserItem" },
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "UserItem" },
-      typeCondition: {
-        kind: "NamedType",
-        name: { kind: "Name", value: "UserQueryData" },
-      },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "email" } },
-          { kind: "Field", name: { kind: "Name", value: "userId" } },
-          { kind: "Field", name: { kind: "Name", value: "username" } },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode<LoginQuery, LoginQueryVariables>;
+export type LoginQuery = { __typename?: 'QueryRoot', user: { __typename?: 'UserQueryData', email: string, userId: any, username: string } };
+
+
+export const LoginDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"login"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"email"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"password"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"user"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"queryEmail"},"value":{"kind":"Variable","name":{"kind":"Name","value":"email"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"FragmentSpread","name":{"kind":"Name","value":"UserItem"}}]}}]}},{"kind":"FragmentDefinition","name":{"kind":"Name","value":"UserItem"},"typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"UserQueryData"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"email"}},{"kind":"Field","name":{"kind":"Name","value":"userId"}},{"kind":"Field","name":{"kind":"Name","value":"username"}}]}}]} as unknown as DocumentNode<LoginQuery, LoginQueryVariables>;
--- api/types/types.generated.ts
+++ api/types/types.generated.ts
@@ -1,14 +1,8 @@
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -22,70 +16,71 @@ export type Scalars = {
 };
 
 export type DbqueryObject = {
-  limit?: InputMaybe<Scalars["Int"]>;
+  limit?: InputMaybe<Scalars['Int']>;
 };
 
 export type DiveQueryData = {
-  __typename?: "DiveQueryData";
-  createdAt: Scalars["NaiveDateTime"];
-  deletedAt?: Maybe<Scalars["NaiveDateTime"]>;
-  deletedBy?: Maybe<Scalars["UUID"]>;
-  depth?: Maybe<Scalars["Float"]>;
-  disciplineType?: Maybe<Scalars["String"]>;
-  distance?: Maybe<Scalars["Float"]>;
-  diveId: Scalars["UUID"];
-  diveName?: Maybe<Scalars["String"]>;
-  diveTime?: Maybe<Scalars["Int"]>;
-  id: Scalars["Int"];
-  isActive: Scalars["Boolean"];
-  sessionId: Scalars["UUID"];
-  updatedAt: Scalars["NaiveDateTime"];
-  userId: Scalars["UUID"];
+  __typename?: 'DiveQueryData';
+  createdAt: Scalars['NaiveDateTime'];
+  deletedAt?: Maybe<Scalars['NaiveDateTime']>;
+  deletedBy?: Maybe<Scalars['UUID']>;
+  depth?: Maybe<Scalars['Float']>;
+  disciplineType?: Maybe<Scalars['String']>;
+  distance?: Maybe<Scalars['Float']>;
+  diveId: Scalars['UUID'];
+  diveName?: Maybe<Scalars['String']>;
+  diveTime?: Maybe<Scalars['Int']>;
+  id: Scalars['Int'];
+  isActive: Scalars['Boolean'];
+  sessionId: Scalars['UUID'];
+  updatedAt: Scalars['NaiveDateTime'];
+  userId: Scalars['UUID'];
 };
 
 export type DiveQueryInput = {
-  createdAt?: InputMaybe<Scalars["NaiveDateTime"]>;
-  depth?: InputMaybe<Scalars["Float"]>;
-  disciplineType?: InputMaybe<Scalars["String"]>;
-  distance?: InputMaybe<Scalars["Float"]>;
-  diveId?: InputMaybe<Scalars["UUID"]>;
-  diveName?: InputMaybe<Scalars["String"]>;
-  diveSession?: InputMaybe<Scalars["UUID"]>;
-  diveTime?: InputMaybe<Scalars["NaiveTime"]>;
-  isActive?: InputMaybe<Scalars["Boolean"]>;
-  updatedAt?: InputMaybe<Scalars["NaiveDateTime"]>;
-  userId?: InputMaybe<Scalars["UUID"]>;
+  createdAt?: InputMaybe<Scalars['NaiveDateTime']>;
+  depth?: InputMaybe<Scalars['Float']>;
+  disciplineType?: InputMaybe<Scalars['String']>;
+  distance?: InputMaybe<Scalars['Float']>;
+  diveId?: InputMaybe<Scalars['UUID']>;
+  diveName?: InputMaybe<Scalars['String']>;
+  diveSession?: InputMaybe<Scalars['UUID']>;
+  diveTime?: InputMaybe<Scalars['NaiveTime']>;
+  isActive?: InputMaybe<Scalars['Boolean']>;
+  updatedAt?: InputMaybe<Scalars['NaiveDateTime']>;
+  userId?: InputMaybe<Scalars['UUID']>;
 };
 
 export type DiveSessionInputData = {
-  endTime: Scalars["NaiveDateTime"];
-  sessionName?: InputMaybe<Scalars["String"]>;
-  startTime: Scalars["NaiveDateTime"];
+  endTime: Scalars['NaiveDateTime'];
+  sessionName?: InputMaybe<Scalars['String']>;
+  startTime: Scalars['NaiveDateTime'];
 };
 
 export type DiveSessionModificationData = {
-  endTime?: InputMaybe<Scalars["NaiveDateTime"]>;
-  isActive?: InputMaybe<Scalars["Boolean"]>;
-  sessionId: Scalars["UUID"];
-  sessionName?: InputMaybe<Scalars["String"]>;
-  startTime?: InputMaybe<Scalars["NaiveDateTime"]>;
+  endTime?: InputMaybe<Scalars['NaiveDateTime']>;
+  isActive?: InputMaybe<Scalars['Boolean']>;
+  sessionId: Scalars['UUID'];
+  sessionName?: InputMaybe<Scalars['String']>;
+  startTime?: InputMaybe<Scalars['NaiveDateTime']>;
 };
 
 export type DiveSessionQueryData = {
-  __typename?: "DiveSessionQueryData";
-  createdAt: Scalars["NaiveDateTime"];
-  deletedAt?: Maybe<Scalars["NaiveDateTime"]>;
-  deletedBy?: Maybe<Scalars["UUID"]>;
+  __typename?: 'DiveSessionQueryData';
+  createdAt: Scalars['NaiveDateTime'];
+  deletedAt?: Maybe<Scalars['NaiveDateTime']>;
+  deletedBy?: Maybe<Scalars['UUID']>;
   dives: Array<DiveQueryData>;
-  endTime: Scalars["NaiveDateTime"];
-  id: Scalars["Int"];
-  isActive: Scalars["Boolean"];
-  sessionId: Scalars["UUID"];
-  sessionName?: Maybe<Scalars["String"]>;
-  startTime: Scalars["NaiveDateTime"];
-  updatedAt: Scalars["NaiveDateTime"];
-  userId: Scalars["UUID"];
+  endTime: Scalars['NaiveDateTime'];
+  id: Scalars['Int'];
+  isActive: Scalars['Boolean'];
+  sessionId: Scalars['UUID'];
+  sessionName?: Maybe<Scalars['String']>;
+  startTime: Scalars['NaiveDateTime'];
+  updatedAt: Scalars['NaiveDateTime'];
+  userId: Scalars['UUID'];
 };
+
 
 export type DiveSessionQueryDataDivesArgs = {
   dbQueryDto?: InputMaybe<DbqueryObject>;
@@ -93,82 +88,88 @@ export type DiveSessionQueryDataDivesArgs = {
 };
 
 export type DiveSessionQueryInput = {
-  createdAt?: InputMaybe<Scalars["NaiveDateTime"]>;
-  endTime?: InputMaybe<Scalars["NaiveDateTime"]>;
-  isActive?: InputMaybe<Scalars["Boolean"]>;
-  sessionId?: InputMaybe<Scalars["UUID"]>;
-  sessionName?: InputMaybe<Scalars["String"]>;
-  startTime?: InputMaybe<Scalars["NaiveDateTime"]>;
-  updatedAt?: InputMaybe<Scalars["NaiveDateTime"]>;
-  userId: Scalars["UUID"];
+  createdAt?: InputMaybe<Scalars['NaiveDateTime']>;
+  endTime?: InputMaybe<Scalars['NaiveDateTime']>;
+  isActive?: InputMaybe<Scalars['Boolean']>;
+  sessionId?: InputMaybe<Scalars['UUID']>;
+  sessionName?: InputMaybe<Scalars['String']>;
+  startTime?: InputMaybe<Scalars['NaiveDateTime']>;
+  updatedAt?: InputMaybe<Scalars['NaiveDateTime']>;
+  userId: Scalars['UUID'];
 };
 
 export type LoginData = {
-  email: Scalars["String"];
-  password: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type MutationRoot = {
-  __typename?: "MutationRoot";
+  __typename?: 'MutationRoot';
   addDiveSession: DiveSessionQueryData;
-  deleteAllDiveSessions: Scalars["Int"];
-  deleteAllUsers: Scalars["Int"];
+  deleteAllDiveSessions: Scalars['Int'];
+  deleteAllUsers: Scalars['Int'];
   insertUser: UserQueryData;
   login: UserQueryData;
-  logout: Scalars["Boolean"];
+  logout: Scalars['Boolean'];
   modifyDiveSession: DiveSessionQueryData;
 };
+
 
 export type MutationRootAddDiveSessionArgs = {
   sessionInputData: DiveSessionInputData;
 };
 
+
 export type MutationRootInsertUserArgs = {
   userData: UserInputData;
 };
 
+
 export type MutationRootLoginArgs = {
   loginData: LoginData;
 };
+
 
 export type MutationRootModifyDiveSessionArgs = {
   sessionInputData: DiveSessionModificationData;
 };
 
 export type QueryRoot = {
-  __typename?: "QueryRoot";
+  __typename?: 'QueryRoot';
   allUsers: Array<UserQueryData>;
   diveSessions: Array<DiveSessionQueryData>;
-  guarded: Scalars["Boolean"];
+  guarded: Scalars['Boolean'];
   user: UserQueryData;
 };
+
 
 export type QueryRootDiveSessionsArgs = {
   dbQueryDto?: InputMaybe<DbqueryObject>;
   diveSessionInput: DiveSessionQueryInput;
 };
 
+
 export type QueryRootUserArgs = {
-  queryEmail: Scalars["String"];
+  queryEmail: Scalars['String'];
 };
 
 export type UserInputData = {
-  email: Scalars["String"];
-  password: Scalars["String"];
-  username: Scalars["String"];
+  email: Scalars['String'];
+  password: Scalars['String'];
+  username: Scalars['String'];
 };
 
 export type UserQueryData = {
-  __typename?: "UserQueryData";
-  createdAt: Scalars["NaiveDateTime"];
-  deletedAt?: Maybe<Scalars["NaiveDateTime"]>;
-  deletedBy?: Maybe<Scalars["UUID"]>;
-  email: Scalars["String"];
-  hashedPassword: Scalars["String"];
-  id: Scalars["Int"];
-  isActive: Scalars["Boolean"];
-  passwordSalt: Array<Scalars["Int"]>;
-  updatedAt: Scalars["NaiveDateTime"];
-  userId: Scalars["UUID"];
-  username: Scalars["String"];
+  __typename?: 'UserQueryData';
+  createdAt: Scalars['NaiveDateTime'];
+  deletedAt?: Maybe<Scalars['NaiveDateTime']>;
+  deletedBy?: Maybe<Scalars['UUID']>;
+  email: Scalars['String'];
+  hashedPassword: Scalars['String'];
+  id: Scalars['Int'];
+  isActive: Scalars['Boolean'];
+  passwordSalt: Array<Scalars['Int']>;
+  updatedAt: Scalars['NaiveDateTime'];
+  userId: Scalars['UUID'];
+  username: Scalars['String'];
 };

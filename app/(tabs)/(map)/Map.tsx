9c4f28671a13eaadee82c163e28aecdad348ac4f
AJTJ <janke.aaron@gmail.com>
2023-07-23 18:49:58 -0600

    update Sun 23 Jul 2023 18:49:58 CST

--- app/(tabs)/(map)/Map.tsx
+++ app/(tabs)/(map)/Map.tsx
@@ -1,11 +1,42 @@
 import { CoreText, LinearGradient } from "@/components";
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import MapLibreGL from "@maplibre/maplibre-react-native";
+
+// Notes
+// https://github.com/maplibre/maplibre-react-native
+
+// Will be null for most users (only Mapbox authenticates this way).
+// Required on Android. See Android installation notes.
+MapLibreGL.setAccessToken(null);
 
 export default function Map() {
   return (
-    <LinearGradient>
-      <CoreText>The Map</CoreText>
-    </LinearGradient>
+    // <LinearGradient>
+
+    <View style={styles.page}>
+      <MapLibreGL.MapView
+        style={styles.map}
+        logoEnabled={false}
+        styleURL="https://demotiles.maplibre.org/style.json"
+      />
+    </View>
+    // </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#F5FCFF",
+  },
+  map: {
+    flex: 1,
+    alignSelf: "stretch",
+  },
+});
